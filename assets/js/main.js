d5a7eeeeb53d0e3201385ca99fe1bc3ee23202ae
SuperCode <supercode@SuperCodes-MacBook-Air.local>
2020-04-25 17:34:00 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,32 @@
+var text = 'Sam is back from codingschool';
+
+console.log(text.substring(0,3))
+
+let sam =text.substring(0,3);
+
+console.log(text.substring(4,6))
+
+let is =text.substring(4,6);
+
+console.log(text.substring(23,29))
+
+let school =text.substring(23,29);
+
+
+
+
+
+
+
+
+
+
+
+
+document.write("<h1>" + sam + "</h1>"+"</br>")
+
+document.write("<h1>" + is + "</h1>"+"</br>")
+
+document.write("<h1>" + school + "</h1>"+"</br>")
+
+document.write("<h1>" + sam +" "+is+" "+school+ "</h1>"+"</br>")
